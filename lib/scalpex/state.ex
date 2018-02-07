732a00af67f94c5f361f87d59816f6baa38fabc2
Marcelo Goncalves <marcelocg@gmail.com>
2018-02-06 23:19:32 -0300

    Go further with the websock API communication

--- lib/scalpex/state.ex
+++ lib/scalpex/state.ex
@@ -1,3 +1,3 @@
 defmodule Scalpex.State do
-  defstruct env: :test, fee: 0.5, min_gain: 0.03
+  defstruct env: :test, fee: 0.5, min_gain: 0.03, session_id: nil
 end
--- lib/scalpex/trader.ex
+++ lib/scalpex/trader.ex
@@ -1,19 +1,40 @@
 defmodule Scalpex.Trader do
   use WebSockex
   require Logger
+  require Record
 
   def start_link(opts \\ []) do
-    IO.puts "Trader start_link"
-    WebSockex.start_link("wss://api_testnet.blinktrade.com/trade/", __MODULE__, %Scalpex.State{}, opts)
+    Logger.info "Trader start_link"
+    {:ok, pid} = WebSockex.start_link("wss://api_testnet.blinktrade.com/trade/", __MODULE__, %Scalpex.State{}, opts)
+    Logger.info "WebSockEx PID=#{inspect pid} SelfPID=#{inspect self()}"
+    {:ok, pid}
   end
 
-  def handle_frame({type, msg}, state) do
-    IO.puts "Received Message - Type: #{inspect type} -- Message:\n#{msg}\nState:\n#{state}"
+  def handle_connect(conn, state) do
+    Logger.info("Connected! Conn= #{inspect conn} State= #{inspect state}")
     {:ok, state}
+  end
+  
+  def handle_frame({type, msg}, state) do
+    Logger.info "Received Message - Type: #{inspect type}\nMessage: #{msg}\nState: #{state.env}"
+    
+    case Poison.decode(msg) do
+      {:error, error} ->
+        Logger.debug(inspect msg)
+        Logger.error(inspect error)
+      {:ok, message} ->
+        process_msg(message, state)
+    end
   end
 
   def handle_cast({:send, {type, msg} = frame}, state) do
-    IO.puts "Sending #{type} frame with payload: #{msg}"
+    Logger.info "Sending #{type} frame with payload: #{msg}"
     {:reply, frame, state}
+  end
+
+  # Initial HeartBeat 
+  defp process_msg(%{"MsgType" => "0"} = msg, state) do
+    Logger.info "Received initial HeartBeat - #{inspect msg}"
+    {:ok, %{state | session_id: msg["SessionID"]}}
   end
 end
