c2cffbb67c3afa7c9bb998e3d38bbf8761235d0e
Marcelo Goncalves <marcelocg@gmail.com>
2018-02-06 23:16:28 -0300

    Rollback to make this a CLI app

--- mix.exs
+++ mix.exs
@@ -15,6 +15,8 @@ defmodule Issues.Mixfile do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
+      # mod: { Scalpex.Application, [] },
+      env: [ env: Mix.env ],
       extra_applications: [:logger]
     ]
   end
@@ -23,6 +25,7 @@ defmodule Issues.Mixfile do
   defp deps do
     [
       {:websockex, "~> 0.4.0"},
+      {:poison, "~> 3.1"},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
