d9e5a1115d6bd78dd295fcda1dbfcb2db2ffe7aa
Marcelo Goncalves <marcelocg@gmail.com>
2018-02-20 11:50:33 -0300

    Test correct action when spread is low

--- test/scalpex/trader_test.exs
+++ test/scalpex/trader_test.exs
@@ -4,6 +4,7 @@ defmodule Scalpex.TraderTest do
 
   test "Responds correctly according to a given spread" do
     assert {:reply, _, _} = Scalpex.Trader.decide_action(10_000, %Scalpex.State{})
+    assert {:ok, _}       = Scalpex.Trader.decide_action(0.001, %Scalpex.State{})
   end
 
 end
